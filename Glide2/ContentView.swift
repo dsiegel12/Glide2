7664917fd621c5e42e45e2b530d9405143c0aafc
DS <dsiegel@smith.edu>
2026-03-13 22:42:51 -0400

    Initial Commit

---
+++ Glide2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Glide2
+//
+//  Created by DonSiegel on 3/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Glide2/Glide2App.swift
@@ -0,0 +1,17 @@
+//
+//  Glide2App.swift
+//  Glide2
+//
+//  Created by DonSiegel on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Glide2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
